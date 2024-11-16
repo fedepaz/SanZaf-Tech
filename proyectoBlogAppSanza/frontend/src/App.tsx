ddef9632066ee37cf46a3964044e0d225a92cae2
fedepaz89 <fedepaz89@gmail.com>
2024-11-16 19:18:51 -0300

    ok, funcionando en dev mode

--- proyectoBlogAppSanza/frontend/src/App.tsx
+++ proyectoBlogAppSanza/frontend/src/App.tsx
@@ -14,7 +14,7 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Router>
+      <Router future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
