88934b485ea3911e3f44ceb1da1f77fef5976486
fedepaz89 <fedepaz89@gmail.com>
2024-11-17 11:03:34 -0300

    comenzando preparativos para deploy

--- proyectoBlogAppSanza/backend/src/server.ts
+++ proyectoBlogAppSanza/backend/src/server.ts
@@ -6,18 +6,14 @@ import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth";
 import postRoutes from "./routes/posts";
 import errorHandler from "./middleware/errorHandler";
+import { configureSecurityMiddleware } from "./middleware/security";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    credentials: true,
-  })
-);
+configureSecurityMiddleware(app);
 app.use(express.json());
 app.use(cookieParser());
 
